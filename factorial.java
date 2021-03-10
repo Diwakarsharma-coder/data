2fb0145ec4bcf65c02fe31f5e2bcf7b05d2d906e
Diwakar Sharma <80200780+Diwakarsharma-coder@users.noreply.github.com>
2021-03-10 18:58:02 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,14 @@
+class factorial
+{
+	public static void main(String args[])
+	{
+		int num=5;
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+		
+	}
+}
